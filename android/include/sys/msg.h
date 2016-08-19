a7c9dd92120acade180c25a4488fbc2f026ec604
Yuexi Ma <yuexima@google.com>
2016-08-19 00:54:37 -0700

    removed msg.h and sem.h
    Because of a recent change in bionic, these files are no longer necessary
    
    Related change ids:
    1878ea08de92f68cd89da76b5368fe81b535a029
    7c59f3f6f3b6dbfcfb261b07062590d2dad2da62
    
    Change-Id: Ib56be68b1894eafffc09ef978fd2515ff13c4847

--- android/include/sys/msg.h
+++
@@ -1,8 +0,0 @@
-#include <linux/msg.h>
-#include <sys/types.h>
-
-int msgget(key_t, int);
-ssize_t msgrcv(int, void *, size_t, long, int);
-int msgsnd(int, const void *, size_t, int);
-int msgctl(int, int, struct msqid_ds *);
-
--- android/include/sys/sem.h
+++
@@ -1,3 +0,0 @@
-#include <linux/sem.h>
-
-int semctl(int, int, int, ...);
