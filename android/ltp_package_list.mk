c0e70a142d279dc9d96c0ddbecd3abf63e741aba
Alistair Strachan <astrachan@google.com>
2018-06-26 09:04:32 -0700

    ANDROID: Re-enable compat16 syscall tests.
    The compat16 syscall variants of numerous tests had been disabled by
    blacklisting a cflag, but whatever the original problem was, it appears
    to have been fixed in AOSP master.
    
    All of the _16 tests build now, and every one completes successfully
    except fchown03_16 (but fchown03 also does not work).
    
    Bug: 37950676
    Bug: 73166481
    Test: built & ran all newly enabled tests on hikey960
    Test: build & ran all newly enabled tests on cuttlefish
    Change-Id: I6c92a5dc7eb48e647fd16ac801a9dc190ec09264
    Signed-off-by: Alistair Strachan <astrachan@google.com>

--- android/ltp_package_list.mk
+++ android/ltp_package_list.mk
@@ -81,9 +81,13 @@ ltp_packages := \
   ltp_chmod05 \
   ltp_chmod07 \
   ltp_chown01 \
+  ltp_chown01_16 \
   ltp_chown02 \
+  ltp_chown02_16 \
   ltp_chown03 \
+  ltp_chown03_16 \
   ltp_chown05 \
+  ltp_chown05_16 \
   ltp_chroot01 \
   ltp_chroot02 \
   ltp_chroot03 \
@@ -245,10 +249,15 @@ ltp_packages := \
   ltp_fchmod07 \
   ltp_fchmodat01 \
   ltp_fchown01 \
+  ltp_fchown01_16 \
   ltp_fchown02 \
+  ltp_fchown02_16 \
   ltp_fchown03 \
+  ltp_fchown03_16 \
   ltp_fchown04 \
+  ltp_fchown04_16 \
   ltp_fchown05 \
+  ltp_fchown05_16 \
   ltp_fchownat01 \
   ltp_fchownat02 \
   ltp_fcntl01 \
@@ -452,13 +461,21 @@ ltp_packages := \
   ltp_getdomainname01 \
   ltp_getdtablesize01 \
   ltp_getegid01 \
+  ltp_getegid01_16 \
   ltp_getegid02 \
+  ltp_getegid02_16 \
   ltp_geteuid01 \
+  ltp_geteuid01_16 \
   ltp_geteuid02 \
+  ltp_geteuid02_16 \
   ltp_getgid01 \
+  ltp_getgid01_16 \
   ltp_getgid03 \
+  ltp_getgid03_16 \
   ltp_getgroups01 \
+  ltp_getgroups01_16 \
   ltp_getgroups03 \
+  ltp_getgroups03_16 \
   ltp_gethostbyname_r01 \
   ltp_gethostname01 \
   ltp_getitimer01 \
@@ -500,7 +517,9 @@ ltp_packages := \
   ltp_gettimeofday01 \
   ltp_gettimeofday02 \
   ltp_getuid01 \
+  ltp_getuid01_16 \
   ltp_getuid03 \
+  ltp_getuid03_16 \
   ltp_getxattr01 \
   ltp_getxattr02 \
   ltp_getxattr03 \
@@ -581,8 +600,11 @@ ltp_packages := \
   ltp_ksm05 \
   ltp_ksm06 \
   ltp_lchown01 \
+  ltp_lchown01_16 \
   ltp_lchown02 \
+  ltp_lchown02_16 \
   ltp_lchown03 \
+  ltp_lchown03_16 \
   ltp_lftest \
   ltp_lgetxattr01 \
   ltp_lgetxattr02 \
@@ -1033,19 +1055,33 @@ ltp_packages := \
   ltp_setegid01 \
   ltp_setegid02 \
   ltp_setfsgid01 \
+  ltp_setfsgid01_16 \
   ltp_setfsgid02 \
+  ltp_setfsgid02_16 \
   ltp_setfsgid03 \
+  ltp_setfsgid03_16 \
   ltp_setfsuid01 \
+  ltp_setfsuid01_16 \
   ltp_setfsuid02 \
+  ltp_setfsuid02_16 \
   ltp_setfsuid03 \
+  ltp_setfsuid03_16 \
   ltp_setfsuid04 \
+  ltp_setfsuid04_16 \
   ltp_setgid01 \
+  ltp_setgid01_16 \
   ltp_setgid02 \
+  ltp_setgid02_16 \
   ltp_setgid03 \
+  ltp_setgid03_16 \
   ltp_setgroups01 \
+  ltp_setgroups01_16 \
   ltp_setgroups02 \
+  ltp_setgroups02_16 \
   ltp_setgroups03 \
+  ltp_setgroups03_16 \
   ltp_setgroups04 \
+  ltp_setgroups04_16 \
   ltp_sethostname01 \
   ltp_sethostname02 \
   ltp_sethostname03 \
@@ -1061,25 +1097,45 @@ ltp_packages := \
   ltp_setpgrp02 \
   ltp_setpriority02 \
   ltp_setregid01 \
+  ltp_setregid01_16 \
   ltp_setregid02 \
+  ltp_setregid02_16 \
   ltp_setregid03 \
+  ltp_setregid03_16 \
   ltp_setregid04 \
+  ltp_setregid04_16 \
   ltp_setresgid01 \
+  ltp_setresgid01_16 \
   ltp_setresgid02 \
+  ltp_setresgid02_16 \
   ltp_setresgid03 \
+  ltp_setresgid03_16 \
   ltp_setresgid04 \
+  ltp_setresgid04_16 \
   ltp_setresuid01 \
+  ltp_setresuid01_16 \
   ltp_setresuid02 \
+  ltp_setresuid02_16 \
   ltp_setresuid03 \
+  ltp_setresuid03_16 \
   ltp_setresuid04 \
+  ltp_setresuid04_16 \
   ltp_setresuid05 \
+  ltp_setresuid05_16 \
   ltp_setreuid01 \
+  ltp_setreuid01_16 \
   ltp_setreuid02 \
+  ltp_setreuid02_16 \
   ltp_setreuid03 \
+  ltp_setreuid03_16 \
   ltp_setreuid04 \
+  ltp_setreuid04_16 \
   ltp_setreuid05 \
+  ltp_setreuid05_16 \
   ltp_setreuid06 \
+  ltp_setreuid06_16 \
   ltp_setreuid07 \
+  ltp_setreuid07_16 \
   ltp_setrlimit01 \
   ltp_setrlimit02 \
   ltp_setrlimit03 \
@@ -1091,9 +1147,13 @@ ltp_packages := \
   ltp_settimeofday01 \
   ltp_settimeofday02 \
   ltp_setuid01 \
+  ltp_setuid01_16 \
   ltp_setuid02 \
+  ltp_setuid02_16 \
   ltp_setuid03 \
+  ltp_setuid03_16 \
   ltp_setuid04 \
+  ltp_setuid04_16 \
   ltp_setxattr01 \
   ltp_setxattr02 \
   ltp_setxattr03 \
